f284df1296ce6fbae8f7015e5e442921fbefd6f0
Derek Song <dereksong28@gmail.com>
2025-05-20 00:10:09 -0400

    github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
+import ghPages from "vite-plugin-gh-pages";
+
 export default defineConfig({
-  plugins: [react(), svgr()],
+  base: "/", 
+  plugins: [react(), svgr(), ghPages()],
 });
