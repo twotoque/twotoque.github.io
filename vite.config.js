63ad56de36807e75971dc56ec4b80ade35e5e112
Derek Song <dereksong28@gmail.com>
2025-05-20 00:11:58 -0400

    Fixed vite config error

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-import ghPages from "vite-plugin-gh-pages";
-
 export default defineConfig({
   base: "/", 
-  plugins: [react(), svgr(), ghPages()],
+  plugins: [react(), svgr()],
 });
